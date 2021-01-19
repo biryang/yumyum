113d06126a24a0305eed2dfb3786176c9be920a1
HappyReni <dnjsvlfwo@gmail.com>
2021-01-20 00:37:36 +0900

    랜덤 api 적용

--- front/js_random.js
+++ front/js_random.js
@@ -1,6 +1,23 @@
 'use strict'
 
+
+const recomMenu = document.querySelector("#item");
+const foodImage = document.querySelector(".foodImage>img");
+
 let url = "http://13.125.227.52:85/random";
-fetch(url)
-    .then((response)=>response.json())
-    .then((data) => console.log(data));
+let foodDB = fetch(url)
+                .then((response)=>{
+                    let a=response.json()
+                    return a})
+                .then(json =>{
+                    recomMenu.innerHTML = json.name;
+                    // foodImage.style.backgroundImage = (`url(${json.img})`);
+                    foodImage.src = `${json.img}`;
+                });
+
+
+
+    
+console.log(foodDB);
+// const item = 
+// item.innerHTML = foodDB.
